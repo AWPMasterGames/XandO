bf6ebd64c1e097c8e25ba7f710aebf96ae90965d
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2023-05-06 19:48:49 +0300

    vinsual_checkwin

---
+++ XandO.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <time.h>
+#include <string>
+#include "XandO.h"
+using namespace std;
+void start();
+int main() {
+	setlocale(LC_ALL, "Ru");
+	Field field;
+	field.start();
+	return 0;
+}
